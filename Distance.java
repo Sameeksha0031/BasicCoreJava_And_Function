e9c14eb98e09e84377c7884ded3e4fda4303acc3
Sameeksha0031 <108124525+Sameeksha0031@users.noreply.github.com>
2022-07-21 13:34:03 +0530

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -7,7 +7,7 @@ public class Distance {
 		int x = sc.nextInt();
 		System.out.println("Enter the point y");
 		int y = sc.nextInt();
-		Double distance = (Double)Math.sqrt(Math.pow(x, 2) + Math.pow(x, 2));
+		Double distance = (Double)Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 		System.out.println("Euclidean distance from the point (x, y) to the origin (0, 0) :" + distance);
 	}
 
